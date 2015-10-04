bfe0c7e851f36920f0149b2a00848dead28b889b
gaochao04 <824310342@qq.com>
2015-10-04 22:07:05 +0800

    change fis-conf

--- fis-conf.js
+++ fis-conf.js
@@ -9,10 +9,6 @@ fis.match('*.less', {
     rExt: '.css'
 });
 
-fis.match('::packager', {
-    postpackager: fis.plugin('loader')  
-});
-
 // 目录编译规范
 fis.match(/^\/page\/(.*\.(tpl|html))/i, {
     release: '/views/web/$1'
@@ -30,10 +26,61 @@ fis.match(/^\/page\/(.*\.mod\.js)/i, {
     isMod: true
 });
 
-fis.match(/^\/public\/libs.*\.(less|css)'/i, {
-  packTo: '/public/pkg/aio.css'
-});
-
 fis.match(/^\/public\/(.*\..*)$/i, {
     release: '/public/web/$1'
 });
+
+
+/**
+ * prod 产品环境配置 js、css 打包配置
+ */
+// 增加md5
+fis.media('prod').match('*.{js,css,less,png}', {
+    useHash: true
+});
+
+// js 、css 压缩配置
+fis.media('prod').match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+});
+fis.media('prod').match('*.{css,less}', {
+    optimizer: fis.plugin('clean-css')
+});
+
+/**
+ * prod 环境打包配置
+ */
+fis.media('prod').match('::packager', {
+    postpackager: fis.plugin('loader')
+});
+
+fis.media('prod').match('/public/libs/third/bootstrap.css', {
+        packTo: '/public/pkg/aio.css'
+    }).media('prod').match('/public/libs/third/highlight/docco.css', {
+        packTo: '/public/pkg/aio.css'
+    }).media('prod').match('/public/libs/third/highlight/docco.css', {
+        packTo: '/public/pkg/aio.css'
+    }).media('prod').match('/public/libs/core/core.less', {
+        packTo: '/public/pkg/aio.css'
+    }).media('prod').match('/public/libs/third/jsmod-extend.less', {
+        packTo: '/public/pkg/aio.css'
+    });
+
+fis.media('prod').match('/public/libs/third/swig.min.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/jquery.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/jsmod.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/jsmod-extend.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/marked.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/highlight/highlight.pack.js', {
+      packTo: '/public/pkg/aio.js'
+    });
